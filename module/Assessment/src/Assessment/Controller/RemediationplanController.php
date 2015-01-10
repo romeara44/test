dd2dbf4236c074906790fc97f3c946a74d0cccbb
malayanton <malayanton@gmail.com>
2015-01-10 17:20:48 +0200

    cvs_version_of_remediation_paln

--- module/Assessment/src/Assessment/Controller/RemediationplanController.php
+++ module/Assessment/src/Assessment/Controller/RemediationplanController.php
@@ -391,9 +391,14 @@ class RemediationplanController extends AbstractActionController
             'Target Date',
         );
         $csvList[] = '';
+        $continue = true;
+        
         foreach ($actions as $rpa) {
             //$rpa->rpa_threat = str_replace('§', utf8_decode('§'), $rpa->rpa_threat);
-
+            if($continue) {
+                $continue = false;
+                continue;
+            }
             $rpa->rpa_threat = str_replace('Â', '', $rpa->rpa_threat);
 
             //echo $rpa->rpa_threat;
