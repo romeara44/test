9f28c47ab1119a96dfaac2dacb869d252b2f0370
alexey_rakul <alexey.rakul@gmail.com>
2016-02-01 13:32:56 +0200

    small fixes

--- module/Assessment/src/Assessment/Model/AssessmentTable.php
+++ module/Assessment/src/Assessment/Model/AssessmentTable.php
@@ -994,8 +994,9 @@ class AssessmentTable implements ServiceLocatorAwareInterface
                 $rpaData['rpa_rp_id'] = $rpId;
 
                 if (empty($arlc[$cat->aqc_ar_id])) {
-                    //$contact_role = $this->getServiceLocator()->get('Client\Model\CompanyRolesTable')->getCompanyRoleByCompanyAndRole($a->a_c_id, $cat->aqc_ar_id);
-                    $rpaData['rpa_contact_u_id'] = 0;
+                    $contact_role = $this->getServiceLocator()->get('Client\Model\CompanyRolesTable')->getCompanyRoleByCompanyAndRole($a->a_c_id, $cat->aqc_ar_id);
+//                    var_dump($contact_role);die();
+                    $rpaData['rpa_contact_u_id'] = $contact_role->cr_u_id;
                 } else {
                     $rpaData['rpa_contact_u_id'] = $arlc[$cat->aqc_ar_id];
                 }
