adbccd23ea5533c5d6bf2e31f5af25e26c55ea73
malayanton <malayanton@gmail.com>
2016-02-08 20:52:44 +0200

    HC-289

--- module/Traininglog/src/Traininglog/Model/TraininglogTable.php
+++ module/Traininglog/src/Traininglog/Model/TraininglogTable.php
@@ -69,17 +69,41 @@ class TraininglogTable implements ServiceLocatorAwareInterface
             $select->join(array('tr' => 'trainers'), new \Zend\Db\Sql\Expression('CONCAT(tl_trainer_type, "_", tl_trainer_id) = CONCAT("trainer", "_", tr.tr_id)'), array(), 'left');
             $select->join(array('u' => 'users'), new \Zend\Db\Sql\Expression('CONCAT(tl_trainer_type, "_", tl_trainer_id) = CONCAT("user", "_", u.u_id)'), array(), 'left');
             $select->join(array('u2' => 'users'), new \Zend\Db\Sql\Expression('tl_create_u_id = u2.u_id'), array(), 'left');
-            $select->join(array('c' => 'companies'), 'tl_company_id = c_id', array('_tl_company_name' => 'c_name'), 'left');
+            $select->join(array('c' => 'companies'), 'tl_company_id = c.c_id', array('_tl_company_name' => 'c_name'), 'left');
+            $select->join(array('c2' => 'companies'), 'u2.u_company_id = c2.c_id', array(), 'left');
 
             if ($orderBy) {
                 $order = $order ? $order : 'ASC';
                 $select->order($orderBy . ' ' . $order);
             }
-            if($identity['u_company_id']) {
-                $select->where("(u2.u_company_id = " . $identity['u_company_id'] . " OR tl_company_id = " . $identity['u_company_id'] . ')');
-            } else {
-                $select->where("u2.u_company_id IS NULL ");
+
+            if ($identity['u_role_id'] != User::ROLE_ADMIN) {
+                if($identity['u_company_id']) {
+                    if($identity['u_role_id'] == User::ROLE_SENIOR_CONSULTANT) {
+                        $select->where("(u2.u_company_id = " . $identity['u_company_id'] 
+                                       . " OR tl_company_id = " . $identity['u_company_id'] 
+                                       . " OR u2.u_senior_consultant_u_id = " . $identity['u_id'] 
+                                       . " OR c2.c_consultant_u_id = " . $identity['u_id'] . ')');
+                    } else if($identity['u_role_id'] == User::ROLE_CONSULTANT) {
+                        $select->where("(u2.u_company_id = " . $identity['u_company_id'] 
+                                       . " OR tl_company_id = " . $identity['u_company_id'] 
+                                       . " OR c2.c_consultant_u_id = " . $identity['u_id'] . ')');
+                    } else {
+                        $select->where("(u2.u_company_id = " . $identity['u_company_id'] . " OR tl_company_id = " . $identity['u_company_id'] . ')');
+                    }
+                } else {
+                    if($identity['u_role_id'] == User::ROLE_SENIOR_CONSULTANT) {
+                        $select->where("(tl_create_u_id = " . $identity['u_id'] . " OR u2.u_senior_consultant_u_id = " . $identity['u_id'] . ')');
+                    } elseif ($identity['u_role_id'] == User::ROLE_CONSULTANT) {
+                        $select->where("(tl_create_u_id = " . $identity['u_id'] 
+                                       . " OR u2.u_senior_consultant_u_id = " . $identity['u_id']
+                                       . " OR c2.c_consultant_u_id = " . $identity['u_id'] . ')');
+                    } else {
+                        $select->where('tl_create_u_id = ' . $identity['u_id']);
+                    }
+                }
             }
+
             $select->group('tl_id');
 
             $paginator = new Paginator($paginatorAdapter);
