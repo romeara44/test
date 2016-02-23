a61ad1e4909b4ef5d5c9dc5fde2cd4864dea95b3
alexey_rakul <alexey.rakul@gmail.com>
2016-02-23 17:42:46 +0200

    fix db name

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -14,7 +14,7 @@
 return array(
     'db' => array(
         'driver'         => 'Pdo',
-        'dsn'            => 'mysql:dbname=hipaa;host=localhost',
+        'dsn'            => 'mysql:dbname=hipaacar_app;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
