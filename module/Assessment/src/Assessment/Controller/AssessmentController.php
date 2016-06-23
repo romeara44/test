6bee651228f091a9ea8ae0963f8af980c2d76c73
alexey_rakul <alexey.rakul@gmail.com>
2016-06-23 17:15:26 +0300

    HC-344, HC-345

--- module/Assessment/src/Assessment/Controller/AssessmentController.php
+++ module/Assessment/src/Assessment/Controller/AssessmentController.php
@@ -347,6 +347,10 @@ class AssessmentController extends AbstractActionController
 
                 $this->getServiceLocator()->get('Assessment\Model\AssessmentQuestionAnswerTable')->saveAqa($id, $adrId, $assessmentRole, $post, $files);
 
+                if ($post['autoSave']) {
+                    return $this->redirect()->toRoute('assessment', array('controller' => 'assessment', 'action' => 'edit', 'id' => $id, 'step' => $post['setStep'], 'locationRole' => $adrId . '_' . $assessmentRole));
+                }
+
                 if ($this->getAssessmentTable()->checkLocationFinished($id, $adrId)) {
                     $this->getAssessmentTable()->_createRemediationPlan($id, $adrId);
                     if ($this->getAssessmentTable()->checkAllLocationsFinished($id)) {
