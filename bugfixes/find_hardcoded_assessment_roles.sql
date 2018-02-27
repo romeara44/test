7753972cbf95839e3fec673da186b778059154d6
Christopher Siegman <christopher.siegman@gmail.com>
2018-02-27 16:59:05 +0000

    Add find and replace for hard-coded role aliases in Assessment Questions table

---
+++ bugfixes/find_hardcoded_assessment_roles.sql
@@ -0,0 +1,20 @@
+SELECT * FROM rockyhil_hipaa.assessments_questions
+WHERE aq_title like "%HR Director%";
+SELECT * FROM rockyhil_hipaa.assessments_questions
+WHERE aq_title like "%Chief Privacy Officer%";
+SELECT * FROM rockyhil_hipaa.assessments_questions
+WHERE aq_title like "%EMR Administrator%";
+SELECT * FROM rockyhil_hipaa.assessments_questions
+WHERE aq_title like "%IT Network Manager%";
+SELECT * FROM rockyhil_hipaa.assessments_questions
+WHERE aq_title like "%Chief Security Officer%";
+SELECT * FROM rockyhil_hipaa.assessments_questions
+WHERE aq_title like "%HIPAA Compliance Officer%";
+SELECT * FROM rockyhil_hipaa.assessments_questions
+WHERE aq_title like "%Facilities Manager%";
+SELECT * FROM rockyhil_hipaa.assessments_questions
+WHERE aq_title like "%Project Manager%";
+SELECT * FROM rockyhil_hipaa.assessments_questions
+WHERE aq_title like "%Approval Authority%";
+SELECT * FROM rockyhil_hipaa.assessments_questions
+WHERE aq_title like "%Signoff Authority%";
---
+++ bugfixes/replace_hardcoded_assessment_roles.sql
@@ -0,0 +1,22 @@
+UPDATE rockyhil_hipaa.assessments_questions
+SET aq_title = REPLACE(aq_title, 'HR Director', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions
+SET aq_title = REPLACE(aq_title, 'Chief Privacy Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions
+SET aq_title = REPLACE(aq_title, 'EMR Administrator', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions
+SET aq_title = REPLACE(aq_title, 'IT Network Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions
+SET aq_title = REPLACE(aq_title, 'Chief Security Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions
+SET aq_title = REPLACE(aq_title, 'Chief Security officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions
+SET aq_title = REPLACE(aq_title, 'HIPAA Compliance Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions
+SET aq_title = REPLACE(aq_title, 'Facilities Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions
+SET aq_title = REPLACE(aq_title, 'Project Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions
+SET aq_title = REPLACE(aq_title, 'Approval Authority', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions
+SET aq_title = REPLACE(aq_title, 'Signoff Authority', '@role_alias');
