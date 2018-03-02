d2ef5f26d5a9e37da5824245fa5986c00af6055a
Christopher Siegman <christopher.siegman@gmail.com>
2018-03-02 06:06:10 +0000

    Add dev version of SQL updates

--- bugfixes/replace_hardcoded_assessment_roles.sql
+++ bugfixes/replace_hardcoded_assessment_roles.sql
@@ -20,3 +20,73 @@ UPDATE rockyhil_hipaa.assessments_questions
 SET aq_title = REPLACE(aq_title, 'Approval Authority', '@role_alias');
 UPDATE rockyhil_hipaa.assessments_questions
 SET aq_title = REPLACE(aq_title, 'Signoff Authority', '@role_alias');
+
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_specification = REPLACE(aqc_specification, 'HR Director', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_specification = REPLACE(aqc_specification, 'Chief Privacy Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_specification = REPLACE(aqc_specification, 'EMR Administrator', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_specification = REPLACE(aqc_specification, 'IT Network Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_specification = REPLACE(aqc_specification, 'Chief Security Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_specification = REPLACE(aqc_specification, 'Chief Security officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_specification = REPLACE(aqc_specification, 'HIPAA Compliance Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_specification = REPLACE(aqc_specification, 'Facilities Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_specification = REPLACE(aqc_specification, 'Project Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_specification = REPLACE(aqc_specification, 'Approval Authority', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_specification = REPLACE(aqc_specification, 'Signoff Authority', '@role_alias');
+
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_policy = REPLACE(aqc_policy, 'HR Director', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_policy = REPLACE(aqc_policy, 'Chief Privacy Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_policy = REPLACE(aqc_policy, 'EMR Administrator', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_policy = REPLACE(aqc_policy, 'IT Network Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_policy = REPLACE(aqc_policy, 'Chief Security Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_policy = REPLACE(aqc_policy, 'Chief Security officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_policy = REPLACE(aqc_policy, 'HIPAA Compliance Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_policy = REPLACE(aqc_policy, 'Facilities Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_policy = REPLACE(aqc_policy, 'Project Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_policy = REPLACE(aqc_policy, 'Approval Authority', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_policy = REPLACE(aqc_policy, 'Signoff Authority', '@role_alias');
+
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_description = REPLACE(aqc_description, 'HR Director', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_description = REPLACE(aqc_description, 'Chief Privacy Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_description = REPLACE(aqc_description, 'EMR Administrator', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_description = REPLACE(aqc_description, 'IT Network Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_description = REPLACE(aqc_description, 'Chief Security Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_description = REPLACE(aqc_description, 'Chief Security officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_description = REPLACE(aqc_description, 'HIPAA Compliance Officer', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_description = REPLACE(aqc_description, 'Facilities Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_description = REPLACE(aqc_description, 'Project Manager', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_description = REPLACE(aqc_description, 'Approval Authority', '@role_alias');
+UPDATE rockyhil_hipaa.assessments_questions_categories
+SET aqc_description = REPLACE(aqc_description, 'Signoff Authority', '@role_alias');
+
