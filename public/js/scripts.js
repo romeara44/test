3a597c2d016d4b194b6312063aa7e3eca9bbcfaf
Christopher Siegman <christopher.siegman@gmail.com>
2018-04-12 20:03:34 +0000

    INCOMPLETE - stashing changes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -33,6 +33,7 @@ jQuery(document).ready(function() {
 
         jQuery(this).fancybox({
             'width': '500',
+            'background': 'red',
             'autoDimensions': false,
             'hideOnContentClick': false,
             'closeBtn': false,
@@ -52,6 +53,30 @@ jQuery(document).ready(function() {
         });
 
     });
+
+    // jQuery('.test-modal').each(function() {
+    //
+    //     jQuery(this).fancybox({
+    //         'width': '500',
+    //         'autoDimensions': false,
+    //         'hideOnContentClick': false,
+    //         'closeBtn': false,
+    //         'helpers' : {
+    //             'overlay' : {
+    //                 'closeClick': false
+    //             }
+    //         },
+    //         'keys' : {
+    //             close  : null
+    //         },
+    //         'beforeShow': function () {
+    //             jQuery.fancybox.wrap.bind("contextmenu", function (e) {
+    //                 return false;
+    //             });
+    //         }
+    //     });
+    //
+    // });
 
 	$('ul#tabs-nav').each(function() {
 		var $active, $content, $links = $(this).find('a');
