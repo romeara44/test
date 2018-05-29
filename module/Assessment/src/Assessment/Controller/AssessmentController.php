268cd313e57e5eba5e91fcf8ff800b905c77d7d3
Christopher Siegman <christopher.siegman@gmail.com>
2018-05-29 20:31:54 +0000

    Implement Data-Tables for Assessment List Page

--- module/Assessment/src/Assessment/Controller/AssessmentController.php
+++ module/Assessment/src/Assessment/Controller/AssessmentController.php
@@ -180,8 +180,12 @@ class AssessmentController extends AbstractActionController
 
         $sortCol = isset($mappingSortCol[$orderBy]) ? $mappingSortCol[$orderBy] : 'a_id';
         $paginator = $this->getAssessmentTable()->getAssessments(true, $sortCol, $order, $this->getIdentity());
-        $paginator->setCurrentPageNumber($page);
-        $paginator->setItemCountPerPage(10);
+
+//        $paginator->setCurrentPageNumber($page);
+//        $paginator->setItemCountPerPage(10);
+
+        $paginator->setCurrentPageNumber(1);
+        $paginator->setItemCountPerPage($paginator->getTotalItemCount());
 
         $view = new ViewModel(array(
             'order_by' => $orderBy,
