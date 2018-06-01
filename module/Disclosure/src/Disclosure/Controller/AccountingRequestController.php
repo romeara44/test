348bebdfc54ff67d19b3af5eaec3222ec10e5bdb
Jim Manton <jrman@risebroadband.net>
2018-06-01 12:20:14 -0600

    Add Accounting to DataTables

--- module/Disclosure/src/Disclosure/Controller/AccountingRequestController.php
+++ module/Disclosure/src/Disclosure/Controller/AccountingRequestController.php
@@ -118,8 +118,8 @@ class AccountingRequestController extends AbstractActionController
         $sortCol   = isset($mappingSortCol[$orderBy]) ? $mappingSortCol[$orderBy] : 'ar_id';
         $paginator = $this->getAccountingRequestTable()->getAccountingRequests(true, $sortCol, $order, $this->getIdentity(), $search, $mappingTypeItem[$roleFilter]);
       
-        $paginator->setCurrentPageNumber($page);
-        $paginator->setItemCountPerPage(10);
+        $paginator->setCurrentPageNumber(1);
+        $paginator->setItemCountPerPage(0);
 
         $view = new ViewModel(array(
             'order_by'    => $orderBy,
